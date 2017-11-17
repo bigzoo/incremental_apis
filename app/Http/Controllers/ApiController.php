6fd7ca1e13c4cdd11025be6e84b5fce40d5fddbc
Chris Nyaga <nyagachris75@gmail.com>
2017-11-17 08:57:39 +0300

    Created an api controller that has methods containing how to respond to queries that other api controllers will inherit from.

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Response;
+
+class ApiController {
+
+    /**
+     * @var int
+     */
+    protected $statusCode = 200;
+
+    /**
+     * @return mixed
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * @param mixed $statusCode
+     * @return $this
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+        return $this;
+    }
+
+    /**
+     * @param string $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function respondNotFound($message = 'Not Found!'){
+        return $this->setStatusCode(404)->respondWithError($message);
+    }
+
+    /**
+     * @param $data
+     * @param array $headers
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function respond($data , $headers= []){
+
+        return response()->json($data , $this->getStatusCode(), $headers);
+
+    }
+
+    /**
+     * @param $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function respondWithError($message){
+        return $this->respond([
+            'error' =>[
+                'message' => $message,
+                'statusCode' => $this->getStatusCode()
+            ]
+        ]);
+    }
+
+}
